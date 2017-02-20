f033f68aa0bbb803220c21b8baef4d6936945782
letrend <simon.trendel@tum.de>
2017-02-20 22:22:45 +0100

    position velocity and force mode working

--- hps-c/src/main.cpp
+++ hps-c/src/main.cpp
@@ -56,9 +56,9 @@ int main() {
 
 	control_Parameters_t controlparams;
 	controlparams.tag = 0;              // sint32
-	controlparams.outputPosMax = 1000;  // sint32
-	controlparams.outputNegMax = -1000; // sint32
-	controlparams.timePeriod = 100; // float32      //in us set time period to avoid error case
+	controlparams.outputPosMax = 500;  // sint32
+	controlparams.outputNegMax = -500; // sint32
+	controlparams.timePeriod = 10; // float32      //in us set time period to avoid error case
 
 	controlparams.radPerEncoderCount =
 			2 * 3.14159265359 / (2000.0 * 53.0);          // float32
@@ -68,25 +68,41 @@ int main() {
 	controlparams.spNegMax = -1000; // float32
 
 	controlparams.params.pidParameters.integral = 0;             // float32
-	controlparams.params.pidParameters.pgain = 100.0;                   // float32
+	controlparams.params.pidParameters.pgain = 1000.0;                   // float32
 	controlparams.params.pidParameters.igain = 0;                   // float32
 	controlparams.params.pidParameters.dgain = 0;                   // float32
 	controlparams.params.pidParameters.forwardGain = 0;       // float32
-	controlparams.params.pidParameters.deadBand = 0;             // float32
-	controlparams.params.pidParameters.IntegralPosMax = 0; // float32
-	controlparams.params.pidParameters.IntegralNegMax = 0; // float32
+	controlparams.params.pidParameters.deadBand = 2;             // float32
+	controlparams.params.pidParameters.IntegralPosMax = 100; // float32
+	controlparams.params.pidParameters.IntegralNegMax = -100; // float32
+	controlparams.params.pidParameters.lastError = 0;
 
-	pidController controller(&controlparams);
-	float setPoint = 10;
-	float pos = setPoint, vel = setPoint;
+	pidController pos_controller(&controlparams), vel_controller(&controlparams), force_controller(&controlparams);
+	pos_controller.setisEnabled(true);
+	vel_controller.setisEnabled(true);
+	force_controller.setisEnabled(true);
+	float setPoint = 0;
+	float pos = setPoint, vel = setPoint, force = setPoint, tendonDisplacement = 0;
 	short control;
 
+	float polyPar[4]= {2, 1,-0.000117,0};
+
 	int iter = 0;
+
+	int controlMode = Force;
 
 	while( loop_count < 60 ) {
 		for(uint i = 0; i<24;i++)
 			frame.TxBuffer[i] = 0;
-		control = controller.outputCalc(pos,setPoint);
+		if(controlMode == Position)
+			control = pos_controller.outputCalc(pos,setPoint);
+		else if(controlMode == Velocity)
+			control = vel_controller.outputCalc(vel,setPoint);
+		else if(controlMode == Force){
+			force = polyPar[0] + polyPar[1] * tendonDisplacement + polyPar[2] * powf(tendonDisplacement, 2.0f) + polyPar[3] * powf(tendonDisplacement, 3.0f);
+			control = force_controller.outputCalc(force,setPoint);
+		}
+
 
 		frame.pwmRef = control;
 
@@ -95,10 +111,11 @@ int main() {
 
 		prepareData(&frame, READ_DATA);
 
-		  pos = frame.actualPosition*controlparams.radPerEncoderCount;
-		  vel = frame.actualVelocity*controlparams.radPerEncoderCount;
+		pos = frame.actualPosition*controlparams.radPerEncoderCount;
+		vel = frame.actualVelocity*controlparams.radPerEncoderCount;
+		tendonDisplacement = frame.springDisplacement/32768.0f;
 
-		  if((iter++)%100==0){
+		if((iter++)%100==0){
 			// control led
 			*(uint32_t *)h2p_lw_led_addr = ~led_mask;
 
@@ -129,7 +146,7 @@ int main() {
 				   "sensor2:             %d\n",
 				   frame.startOfFrame, control, frame.controlFlags1, frame.controlFlags2, frame.dummy,
 				   pos, vel, frame.actualCurrent, frame.springDisplacement, frame.sensor1, frame.sensor2);
-		  }
+		}
 
 
 
