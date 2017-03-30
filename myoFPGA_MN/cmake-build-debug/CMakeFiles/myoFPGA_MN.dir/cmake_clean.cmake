c497b20015d071a9557de03fef71e17ff9105925
letrend <simon.trendel@tum.de>
2017-03-30 14:38:40 +0200

    with older version of cross compiler

---
+++ myoFPGA_MN/cmake-build-debug/CMakeFiles/myoFPGA_MN.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myoFPGA_MN.dir/src/main.cpp.o"
+  "CMakeFiles/myoFPGA_MN.dir/src/myoMaster.cpp.o"
+  "CMakeFiles/myoFPGA_MN.dir/src/UDPSocket.cpp.o"
+  "CMakeFiles/myoFPGA_MN.dir/src/interface.cpp.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/apps/common/src/obdcreate/obdcreate.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/apps/common/src/eventlog/eventlog.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/apps/common/src/eventlog/eventlogstring.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/apps/common/src/system/system-linux.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/apps/common/src/pcap/pcap-console.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/contrib/console/printlog.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/contrib/getopt/getopt.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/contrib/console/console-linux.c.o"
+  "CMakeFiles/myoFPGA_MN.dir/home/roboy/Downloads/openPowerLink/apps/common/objdicts/CiA302-4_MN/obdpi.c.o"
+  "myoFPGA_MN.pdb"
+  "myoFPGA_MN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/myoFPGA_MN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
